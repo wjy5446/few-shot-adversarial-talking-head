ddcf466ed233ecc6b6560137cf4d93b292b338dc
wjy5446 <wjy5446@gmail.com>
2019-09-01 16:57:20 +0900

    add generator

--- FATH/modules.py
+++ FATH/modules.py
@@ -3,24 +3,22 @@ from layers import *
 import tensorflow as tf
 
 
-def ResBlock_adaIN(x, style_mean, style_std, channels, use_bias=True, sn=True, scope='resblock'):
+def ResBlock_adaIN(x, style_front, style_back, channels, use_bias=True, sn=True, scope='resblock'):
     with tf.variable_scope(scope):
         # x (B, H, W, C)
-        # style_mean (B, C)
-        # style_std (B, C)
-        
+        # style front (style_mean, style_std)
+        # style back (style_mean, style_std) 
         x_init = x
         with tf.variable_scope('res0'):
-            out = adaIN(x, style_mean=style_mean, style_std=style_std)
+            out = adaIN(x, style_mean=style_front[0], style_std=style_front[1])
             out = relu(out)
             out = conv(x, channels, 3, 1, 1, pad_type='reflect', use_bias=use_bias, sn=sn)
         
         with tf.variable_scope('res1'):
-            out = adaIN(out, style_mean=style_mean, style_std=style_std)
+            out = adaIN(out, style_mean=style_back[0], style_std=style_back[1])
             out = relu(out)
             out = conv(out, channels, 3, 1, 1, pad_type='reflect', use_bias=use_bias, sn=sn)
        
-        print(out)
         return out + x_init
 
 def ResBlockDown_g(x, channels, use_bias=True, sn=True, is_training=True, scope='resblock_down_g'): 
@@ -42,8 +40,6 @@ def ResBlockDown_g(x, channels, use_bias=True, sn=True, is_training=True, scope=
             out_l = conv(x, channels, kernel=1, stride=1, pad=0, pad_type='zero', use_bias=use_bias, sn=sn)
             out_l = avg_pooling(out_l)
        
-        print(out_r)
-        print(out_l)
         return out_r + out_l
 
 def ResBlockDown_d(x, channels, use_bias=True, sn=True, scope='resblock_down_d'):
@@ -63,24 +59,22 @@ def ResBlockDown_d(x, channels, use_bias=True, sn=True, scope='resblock_down_d')
             out_l = conv(x, channels, kernel=1, stride=1, pad=0, pad_type='zero', use_bias=use_bias, sn=sn)
             out_l = avg_pooling(out_l)
        
-        print(out_r)
-        print(out_l)
         return out_r + out_l
 
-def ResBlockUp_adaIN(x, style_mean, style_std, channels, use_bias=True, sn=True, scope='resblock_up'): 
+def ResBlockUp_adaIN(x, style_front, style_back, channels, use_bias=True, sn=True, scope='resblock_up'): 
     with tf.variable_scope(scope):
         # x (B, H, W, C)
-        # style_mean (B, C)
-        # style_std (B, C)
+        # style front (style_mean, style_std)
+        # style back (style_mean, style_std) 
 
         with tf.variable_scope('res0_r'):
-            out_r = adaIN(x, style_mean=style_mean, style_std=style_std)
+            out_r = adaIN(x, style_mean=style_front[0], style_std=style_front[1])
             out_r = relu(out_r)
             out_r = up_sampling_nn(out_r)
             out_r = conv(out_r, channels, kernel=3, stride=1, pad=1, pad_type='reflect', use_bias=use_bias, sn=sn)
 
         with tf.variable_scope('res1_r'):
-            out_r = adaIN(out_r, style_mean=style_mean, style_std=style_std)
+            out_r = adaIN(out_r, style_mean=style_back[0], style_std=style_back[1])
             out_r = relu(out_r) 
             out_r = conv(out_r, channels, kernel=3, stride=1, pad=1, pad_type='reflect', use_bias=use_bias, sn=sn)
         
@@ -121,7 +115,6 @@ def SelfAttention(x, sn=True, scope='attention'):
 def adaIN(x, style_mean, style_std, eps=1e-5, scope='adain'):
     with tf.variable_scope(scope):
         # x (B, H, W, C)
-        # psi = [style_mean, style_std]
         # style_mean (B, C)
         # style_std (B, C)
         
@@ -135,23 +128,20 @@ def adaIN(x, style_mean, style_std, eps=1e-5, scope='adain'):
         # x (B, 1, 1, C)
         gamma = tf.reshape(style_mean, [B, 1, 1, C]) # gamma (B, 1, 1, C)
         beta = tf.reshape(style_std, [B, 1, 1, C]) # beta (B, 1, 1, C)
-        print(gamma)
-        print(beta)
         return gamma * x + beta
 
 
 if __name__ == '__main__':
     x = tf.random.normal([4, 6, 6, 512])
-    psi = tf.random.normal([4, 512])
-
+    psi = tf.random.normal([4, 512 * 4])
 
     x_shape = x.get_shape() 
 
     print('[INFO] adaIN')
-    adaIN(x, psi, psi)
+    adaIN(x, psi[:, :512], psi[:, 512:512*2])
     
     print('[Info] resblock adaIN')
-    ResBlock_adaIN(x, psi, psi, x_shape[-1])
+    ResBlock_adaIN(x, (psi[:, :512], psi[:, 512:512*2]), (psi[:, 512*2:512*3], psi[:, 512*3:]), x_shape[-1])
     
     print('[Info] resblock down for generator')
     ResBlockDown_g(x, x_shape[-1])
@@ -160,7 +150,7 @@ if __name__ == '__main__':
     ResBlockDown_d(x, x_shape[-1])
     
     print('[Info] resblock up')
-    ResBlockUp_adaIN(x, psi, psi, x_shape[-1])
+    ResBlockUp_adaIN(x, (psi[:, :512],  psi[:, 512:512*2]), (psi[:, 512*2:512*3], psi[:, 512*3:]), x_shape[-1])
     
     print('[Info] self-attention')
     SelfAttention(x)
