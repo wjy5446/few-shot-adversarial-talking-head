9eb245bd6cc7f73a4f7e7b167ac5834379a341cd
wjy5446 <wjy5446@gmail.com>
2019-09-07 01:11:51 +0900

    add build function

--- FATH/model.py
+++ FATH/model.py
@@ -8,7 +8,8 @@ from keras_vggface.vggface import VGGFace
 from layers import *
 from loss import *
 from modules import *
-
+from utils import *
+from param import *
 
 class FATH(object):
     def __init__(self, sess=None, args=None):
@@ -24,15 +25,12 @@ class FATH(object):
         self.iteration = args.iteration
         self.batch_size = args.batch_size
         self.print_freq = args.print_freq
+        self.save_freq = args.save_freq
+
         self.k = args.k
         self.img_size = args.img_size
         self.c_dim = args.c_dim
         self.e_dim = args.e_dim
-
-        self.smaple_num = args.sample_num
-        self.test_num = args.test_num
-
-        # custom
         self.n_video = args.n_video
 
         # train
@@ -42,17 +40,15 @@ class FATH(object):
         self.beta2 = args.beta2
 
         # dataset
-        self.data = ?
+        self.data = [1, 2]
         self.dataset_num = len(self.data)
 
-        self.sample_dir = os.path.join(self.sample_dir, self.model_dir)
-        check_folder(self.sample_dir)
-        
+       
         # model
         self.embedder = Embedder('embedder')
         self.generator = Generator('generator')
         self.discriminator = Discriminator('discriminator')
-        self.vgg = Vgg19()
+        self.vgg19 = Vgg19()
         self.vggface = VggFace()
 
     def __call__(self, ):
@@ -63,23 +59,23 @@ class FATH(object):
        
         self.inputs_image = tf.placeholder(tf.float32, [self.batch_size, self.k + 1, self.img_size, self.img_size, self.c_dim], name='real_images')
         self.inputs_landmark = tf.placeholder(tf.float32, [self.batch_size, self.k + 1, self.img_size, self.img_size, self.c_dim], name='real_landmarks')
-        self.index = tf.placeholder(tf.float32, [self.batch_size], name='real_index')
+        self.index = tf.placeholder(tf.int32, [self.batch_size], name='real_index')
         
 
         """graph flow"""
-        images = tf.slice(self.inputs_image, [0, 0, 0, 0, 0], [-1, K, -1, -1, -1])
-        landmarks = tf.slice(self.inputs_landmark, [0, 0, 0, 0, 0], [-1, K, -1, -1, -1])
-        t_images = tf.slice(self.inputs_image, [0, K, 0, 0, 0], [-1, 1, -1, -1, -1])
-        t_landmarks = tf.slice(self.inputs_landmark, [0, K, 0, 0, 0], [-1, 1, -1, -1, -1])
+        images = tf.slice(self.inputs_image, [0, 0, 0, 0, 0], [-1, self.k, -1, -1, -1])
+        landmarks = tf.slice(self.inputs_landmark, [0, 0, 0, 0, 0], [-1, self.k, -1, -1, -1])
+        t_images = tf.slice(self.inputs_image, [0, self.k, 0, 0, 0], [-1, 1, -1, -1, -1])
+        t_landmarks = tf.slice(self.inputs_landmark, [0, self.k, 0, 0, 0], [-1, 1, -1, -1, -1])
         
-        x = tf.reshape(images, [self.batch_size * self.k, self.image_size, self.image_size, self.c_dim])
-        y = tf.reshape(landmarks, [self.batch_size * self.k, self.image_size, self.image_size, self.c_dim])
+        x = tf.reshape(images, [self.batch_size * self.k, self.img_size, self.img_size, self.c_dim])
+        y = tf.reshape(landmarks, [self.batch_size * self.k, self.img_size, self.img_size, self.c_dim])
         
-        x_t = tf.reshape(t_images, [self.batch_size, self.image_size, self.image_size, self.c_dim])
-        y_t = tf.reshape(t_landmarks, [self.batch_size, self.image_size, self.image_size, self.c_dim])
+        x_t = tf.reshape(t_images, [self.batch_size, self.img_size, self.img_size, self.c_dim])
+        y_t = tf.reshape(t_landmarks, [self.batch_size, self.img_size, self.img_size, self.c_dim])
 
         e_vectors = self.embedder(x, y)
-        e_vectors = tf.reshape(e_vector, [self.batch_size, self.k, self.e_dim])
+        e_vectors = tf.reshape(e_vectors, [self.batch_size, self.k, self.e_dim])
         e = tf.reduce_mean(e_vectors, axis=1)
 
         # generate fake image
@@ -95,35 +91,132 @@ class FATH(object):
         loss_cnt = LossCnt(x_t, x_fake, self.vgg19, self.vggface) 
         loss_adv = LossAdv(score_fake, li_fm_real, li_fm_fake) 
         loss_match = LossMatch(e, W, self.index)
-        loss_d = LossD(score_real, fake_score)
-        loss_G = loss_cnt + loss_adv + loss_match
-        loss_D = loss_d
+        loss_d = LossD(score_real, score_fake)
+        self.loss_G = loss_cnt + loss_adv + loss_match
+        self.loss_D = loss_d
 
-    
+        print(self.loss_G, self.loss_D)
+        
+        # image
+        self.real_images = x_t
+        self.real_landmark = y_t
+        self.fake_images = x_fake
+
         # get trainable variable
         t_vars = tf.trainable_variables()
         g_vars = [var for var in t_vars if 'discriminator' in var.name]
         d_vars = [var for var in t_vars if 'generator' in var.name]
 
-        self.g_optim = tf.train.AdamOptimizer(self.g_learning_rate, beta1=self.beta1, beta2=self.beta2).minimize(loss_G, var_list=g_vars)
-        self.d_optim = tf.train.AdamOptimizer(self.d_learning_rate, beta1=self.beta1, beta2=self.beta2).minimize(loss_D, var_list=d_vars)
-        
+        self.g_optim = tf.train.AdamOptimizer(self.g_learning_rate, beta1=self.beta1, beta2=self.beta2).minimize(self.loss_G, var_list=g_vars)
+        self.d_optim = tf.train.AdamOptimizer(self.d_learning_rate, beta1=self.beta1, beta2=self.beta2).minimize(self.loss_D, var_list=d_vars)
 
 
-        pass
+        # summary
+        self.summary_loss_cnt = tf.summary.scalar('loss_cnt', loss_cnt)
+        self.summary_loss_adv = tf.summary.scalar('loss_adv', loss_adv)
+        self.summary_loss_match = tf.summary.scalar('loss_match', loss_match)
+        self.summary_loss_G = tf.summary.scalar('loss_G', self.loss_G)
+        self.summary_loss_D = tf.summary.scalar('loss_D', self.loss_D)
+
+
+        list_summary_g = [self.summary_loss_cnt, self.summary_loss_adv, self.summary_loss_match, self.summary_loss_G]
+        list_summary_d = [self.summary_loss_D]
+
+        self.summary_G = tf.summary.merge(list_summary_g)
+        self.summary_D = tf.summary.merge(list_summary_d)
 
     def train(self):
-        pass
+        
+        tf.global_variables_initializer().run()
+
+        self.saver = tf.train.Saver()
+
+        self.write = tf.summary.FileWriter(self.log_dir + '/' + self.model_dir, self.sess.graph)
+        
+        is_load, checkpoint_counter = self.load(self.checkpoint_dir)
+
+        if is_load:
+            start_epoch = (int)(checkpoint_counter . self.iteration)
+            start_batch_id = checkpoint_counter - start_epoch * self.iteration
+            counter = checkpoint_counter
+            print('[*] Load SUCCESS')
+        else:
+            start_epoch = 0
+            start_batch_id = 0
+            counter = 1
+            print('[!] Load fail..')
+
+        start_time = time.time()
+        
+        for epoch in range(start_epoch, self.epoch):
+            for idx in range(star_batch_id, self.iteration):
+
+                # Update G
+                real_images, fake_images, _, loss_g, summary_str = self.sess.run([self.x_t, self.fake_x, self.g_optim, self.loss_G, self.summary_loss_G])
+                self.write.add_summary(summary_str, counter)
+
+                # Update D
+                _, loss_d, summary_str = self.sess.run([self.d_optim, self.loss_d, self.summary_loss_D])
+                self.write.add_summary(summary_str, counter)
+
+                # display
+                counter += 1
+                print('Epoch: [%2d], [%5d/%5d] time: %4.4f loss_g: %.8f, loss_d: %8f' % (epoch, idx, self.iteration, time.time() - start_time,
+                                                                                         loss_g, loss_d))
+
+                if (idx + 1) % self.print_freq == 0:
+                    pass
+
+                
+                if (idx + 1) % self.print_save == 0:
+                    self.save(self.checkpoint_dir, counter)
+
     
     def test(self):
-        pass
+        tf.global_variables_initializer().run()
 
-    def load(self, checkpoint_dir):
-        pass
+        self.saver = tf.train.Saver()
+        is_load, checkpoint_counter = self.load(self.checkpoint_dir)
+
+        if is_load:
+            print('[*] Load Success')
+        else:
+            print('[*] Load failed')        
+
+
 
     def visualize_result(self, epoch):
         pass
+    
+    
+    def model_dir(self):
+        return 'model'
+    
+    def save(self, checkpoint_dir, step):
+        checkpoint_dir = os.path.join(checkpoint_dir, self.model_dir)
 
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+
+        self.saver.save(self.sess, os.path.join(checkpoint_dir, self.model_name + '.model'), global_step=step)
+    
+    def load(self, checkpoint_dir):
+        import re
+
+        print('[*] Reading checkpoints')
+        checkpoint_dir = os.path.join(checkpoint_dir, self.model_dir)
+
+        ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+
+        if ckpt and ckpt.model_checkpoint_path:
+            ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+            self.saver.restore(self.sess, os.path.join(checkpoint_dir, ckpt_name))
+            counter = int(next(re.finditer("(\d+)(?!.*\d)", ckpt_name)).group(0))
+            print('[*] Success to read {}'.format(ckpt_name))
+            return True, counter
+        else:
+            print('[*] Failed to find a checkpoint')
+            return False, 0
 ################################################
 # Embedder
 ################################################
@@ -243,7 +336,7 @@ class Generator:
                     ch = ch // 2
 
             out = SelfAttention(out, scope='attention_1')
-            print(out)
+            
             for i in range(9, 11):
                 front_style = (tf.slice(e_psi, [0, style_info[(4*i)+0][0]], [-1, style_info[(4*i)+0][1]]), 
                               tf.slice(e_psi, [0, style_info[(4*i)+1][0]], [-1, style_info[(4*i)+1][1]]))
@@ -300,8 +393,6 @@ class Discriminator:
                 w0 = tf.get_variable('w0', shape=[1, 512], dtype=tf.float32, initializer=tf.random_normal_initializer())
                 b = tf.get_variable('b', shape=[1], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
                 
-                print(W)
-
                 W_i = tf.gather(W, idx)
                 W = tf.expand_dims(W_i + w0, 2)
                
@@ -395,46 +486,53 @@ class VggFace(tf.keras.Model):
         return out
 
 if __name__ == '__main__':
-    x = tf.random_normal([4, 256, 256, 3])
-    y = tf.random_normal([4, 256, 256, 3])
-    i = [0, 1, 2, 3]   
+#    x = tf.random_normal([4, 256, 256, 3])
+#    y = tf.random_normal([4, 256, 256, 3])
+#    i = [0, 1, 2, 3]   
+#
+#    
+#    print("[INFO] embedder")
+#    embedder = Embedder('embedder')
+#    e = embedder(x, y)
+#
+#    print("[INFO] generator")
+#    generator = Generator('generator')
+#    fake = generator(y, e)    
+#
+#    print("[INFO] discriminator")
+#    discriminator = Discriminator('discriminator')
+#    r_real, fm_real = discriminator(x, y, i)
+#    
+#    r_fake, fm_fake = discriminator(fake, y, i, reuse=True)
+#
+#    print("[INFO] VGG")
+#    vgg19 = Vgg19()
+#    out = vgg19(x)
+#
+#    print("[INFO] VGGFACE")
+#    vggface = VggFace()
+#    out = vggface(x)
+#
+#    print('[INFO] LossCnt')
+#    loss1 = LossCnt(x, fake, vgg19, vggface)
+#    print(loss1)
+#
+#    print('[INFO] LossAdv')
+#    loss2 = LossAdv(r_fake, fm_real, fm_fake)
+#    print(loss2)
+#
+#    print('[INFO] Loss Match')
+#    with tf.variable_scope('discriminator', reuse=True):
+#        with tf.variable_scope('projection'):
+#            W = tf.get_variable('W')
+#
+#    print('[INFO] Loss D')
+#    loss4 = LossD(r_real, r_fake)
+#    print(loss4)
 
-    
-    print("[INFO] embedder")
-    embedder = Embedder('embedder')
-    e = embedder(x, y)
+    args = parse_args()
 
-    print("[INFO] generator")
-    generator = Generator('generator')
-    fake = generator(y, e)    
+    sess = tf.Session()
 
-    print("[INFO] discriminator")
-    discriminator = Discriminator('discriminator')
-    r_real, fm_real = discriminator(x, y, i)
-    
-    r_fake, fm_fake = discriminator(fake, y, i, reuse=True)
-
-    print("[INFO] VGG")
-    vgg19 = Vgg19()
-    out = vgg19(x)
-
-    print("[INFO] VGGFACE")
-    vggface = VggFace()
-    out = vggface(x)
-
-    print('[INFO] LossCnt')
-    loss1 = LossCnt(x, fake, vgg19, vggface)
-    print(loss1)
-
-    print('[INFO] LossAdv')
-    loss2 = LossAdv(r_fake, fm_real, fm_fake)
-    print(loss2)
-
-    print('[INFO] Loss Match')
-    with tf.variable_scope('discriminator', reuse=True):
-        with tf.variable_scope('projection'):
-            W = tf.get_variable('W')
-
-    print('[INFO] Loss D')
-    loss4 = LossD(r_real, r_fake)
-    print(loss4)
+    fath = FATH(sess, args)
+    fath.build_model()
